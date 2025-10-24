0d50814a433a6b6bd794b79deb05651e295e325a
Ccyy615 <chinyi615@gmail.com>
2025-10-24 16:50:02 -0400

    Java Milestone

---
+++ src/test/java/com/example/MilestoneProject/MilestoneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MilestoneProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilestoneProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
